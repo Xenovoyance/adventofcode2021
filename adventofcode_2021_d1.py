84b3c2d44628722a9da6e6e21f6675620d37e85b
Martin Karlsson <xenovoyance@gmail.com>
2021-12-06 11:20:36 +0100

    Test push

--- adventofcode_2021_d1.py
+++ adventofcode_2021_d1.py
@@ -12,6 +12,17 @@ def part_one():
             last_line = int(line)
         return(str(greater))
 
+def part_two():
+    with open(input, "r") as f:
+        lines = f.readlines()
+        last_line = greater = 0
+        for line in lines:
+            if last_line < int(line): 
+                if last_line != 0: greater += 1
+            last_line = int(line)
+        return(str(greater))
+
 if __name__ == "__main__":
     start_time = time.time()
     print("Part 1: " + part_one() + " executed in " + str(time.time() - start_time) + " seconds")
+    print("Part 2: " + part_two() + " executed in " + str(time.time() - start_time) + " seconds")
