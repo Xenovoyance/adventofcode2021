037391a8bda5e71bc3c1773cb6dbfda97a38ff9b
Martin Karlsson <xenovoyance@gmail.com>
2021-12-06 11:15:32 +0100

    >Meep

--- adventofcode_2021_d1.py
+++ adventofcode_2021_d1.py
@@ -2,18 +2,16 @@ import time
 
 input = "adventofcode_2021_d1_input.txt"
 
-def main():
+def part_one():
     with open(input, "r") as f:
         lines = f.readlines()
-        last_line = 0
-        greater = 0
+        last_line = greater = 0
         for line in lines:
             if last_line < int(line): 
                 if last_line != 0: greater += 1
             last_line = int(line)
-        return(greater)
+        return(str(greater))
 
 if __name__ == "__main__":
     start_time = time.time()
-    print("Part 1:", main(), "(", time.time() - start_time, "seconds)")
-    
+    print("Part 1: " + part_one() + " executed in " + str(time.time() - start_time) + " seconds")
