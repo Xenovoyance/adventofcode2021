8779e6d395850c5dca663ca5c9b375740a8fba53
Martin Karlsson <xenovoyance@gmail.com>
2021-12-09 11:08:55 +0100

    Day 2 done

--- day 2/adventofcode_2021_d2.py
+++ day 2/adventofcode_2021_d2.py
@@ -1,25 +1,47 @@
 import time
-import more_itertools
 
 def input_to_string():
     input = "adventofcode_2021_d2_input.txt"
     input_string = []
     with open(input, "r") as f:
         for line in f.readlines():
-            input_string.append(int(line))
+            input_string.append(line.strip().split())
     return input_string
 
 def part_one():
     input_string = input_to_string()
     depth = horizontal = 0
-    return(input_string)
+    
+    for sub_move_key, sub_move_amount in input_string:
+        if sub_move_key == "forward":
+            horizontal += int(sub_move_amount)
+        elif sub_move_key == "down":
+            depth += int(sub_move_amount)
+        elif sub_move_key == "up":
+            depth -= int(sub_move_amount)
+    
+    return str(depth * horizontal)
 
 def part_two():
     input_string = input_to_string()
+    depth = horizontal = aim = 0
+    
+    for sub_move_key, sub_move_amount in input_string:
+        if sub_move_key == "forward":
+            horizontal += int(sub_move_amount)
+            depth += (aim * int(sub_move_amount))
+        elif sub_move_key == "down":
+            aim += int(sub_move_amount)
+        elif sub_move_key == "up":
+            aim -= int(sub_move_amount)
+    
+    return str(depth * horizontal)
         
 
 if __name__ == "__main__":
     start_time = time.time()
-    print("Part 1: " + part_one() + " executed in " + (str((time.time() - start_time)* 1000)) + " ms")
-    #start_time = time.time()
-    #print("Part 2: " + part_two() + " executed in " + (str((time.time() - start_time)*1000)) + " ms")
+    print("Part 1: " + part_one() + " executed in " 
+        + (str((time.time() - start_time)* 1000)) + " ms")
+    start_time = time.time()
+    print("Part 2: " + part_two() + " executed in " 
+        + (str((time.time() - start_time)*1000)) + " ms")
