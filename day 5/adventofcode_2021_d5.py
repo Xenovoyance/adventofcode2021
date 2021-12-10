2b781b357724fd8d4c1677d0eaddd78d90d53b60
Martin Karlsson <xenovoyance@gmail.com>
2021-12-11 00:44:47 +0100

    Day 5 init

---
+++ day 5/adventofcode_2021_d5.py
@@ -0,0 +1,9 @@
+import time
+
+def part_one():
+    print("Yeah")
+
+if __name__ == "__main__":
+    #start_time = time.time()
+    part_one()
+    #print("Executed in " + (str((time.time() - start_time)* 1000)) + " ms")
